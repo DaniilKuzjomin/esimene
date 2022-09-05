9dd855678c3fadad7428ef0f3089e674b8806b92
Daniil Kuzjomin <93324329+DaniilKuzjomin@users.noreply.github.com>
2022-09-05 21:47:19 +0300

    Update Kutsekooliopilane.cs

--- esimene/Kutsekooliopilane.cs
+++ esimene/Kutsekooliopilane.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,29 +11,33 @@ namespace esimene
         public string oppeasutus;
         public string eriala;
         public string kursus;
-        public string toetus;
+        public int toetus;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public void arvutaSissetulek()
+        public Kutsekooliopilane(string oppeasutus, string eriala, int kursus, int toetus, string klass, string spetsialiseerumine, string nimi, int sunniaasta, sugu InimSugu, double maksuvaba, double palk) : base(nimi, sunniaasta, InimSugu, maksuvaba, palk)
         {
-            throw new NotImplementedException();
+            this.oppeasutus = oppeasutus;
+            this.eriala = eriala;
+            this.kursus = kursus;
+            this.toetus = toetus;
         }
 
+        public int arvutaVanus()
+        {
+            int praeguneAasta = DateTime.Now.Year;
+            int vanus = praeguneAasta - sunniaasta;
+            return vanus;
+        }
+
+
+        public override double arvSisse(double maksuvaba, double tulumaks, double palk)
+        {
+            double netopalk = ((palk - maksuvaba) * (1 - (tulumaks / 100))) + maksuvaba;
+            return netopalk;
+        }
         public override void printInfo()
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Tema õppeasutus on {oppeasutus} ja tema eriala on {eriala}, ta on kursusel #{kursus} ja tema toetus on {toetus}, tema töötasu on {arvSisse(palk, maksuvaba, tulumaks)}, tema nimi on {nimi}, ta on {InimSugu} ja tema vanus on {arvutaVanus()}");
         }
+
     }
 }
