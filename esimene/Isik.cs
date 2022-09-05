16468b3d3e3f6e2cc27c74ca2f5f9ce27c7a0687
Daniil Kuzjomin <93324329+DaniilKuzjomin@users.noreply.github.com>
2022-09-05 21:46:58 +0300

    Update Isik.cs

--- esimene/Isik.cs
+++ esimene/Isik.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,10 @@ using System.Threading.Tasks;
 
 namespace esimene
 {
-    abstract class Isik
+    public abstract class Isik
     {
         public string nimi;
         public int sunniaasta;
-        public int vanus;
         public enum sugu { mees, naine }
         public sugu InimSugu;
         public double sissetulek;
@@ -18,43 +17,22 @@ namespace esimene
         public double palk;
         public double tulumaks;
 
-        /*public Isik(sugu inimSugu, string nimi = "---", int sunniaasta=0)
-        {
-            this.nimi = nimi;
-            this.sunniaasta = sunniaasta;
-            this.InimSugu=inimSugu;
-        }*/
 
-        public Isik(string nimi, int sunniaasta, int vanus, sugu inimSugu, int maksuvaba, int tulumaks)
-        {
-            this.nimi = nimi;
-            this.sunniaasta = sunniaasta;
-            this.vanus = vanus;
-            InimSugu = inimSugu;
-        }
-        
         public Isik() { }
 
-        protected Isik(string nimi, int sunniaasta, sugu sugu, int maksuvaba, int palk)
+        public Isik(string nimi, int synniaasta, sugu Inimsugu, double maksuvaba, double palk)
         {
             this.nimi = nimi;
             this.sunniaasta = sunniaasta;
             this.InimSugu = InimSugu;
+            this.maksuvaba = maksuvaba;
+            this.palk = palk;
         }
 
         public abstract void printInfo();
 
-        public int arvutaVanus()
-        {
-            int praeguneAasta = DateTime.Now.Year;
-            int vanus = praeguneAasta - sunniaasta;
-            return vanus;
-        }
 
-        public void arvutaSissetulek()
-        {
-            
-        }
+        public abstract double arvSisse(double palk, double tulumaks, double maksuvaba);
 
         public void muudaNimi(string uusNimi) { nimi = uusNimi; }
     }
