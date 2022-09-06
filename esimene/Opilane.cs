ee0169c8fb35af48da3cb6d3d544d41665d9a977
Daniil Kuzjomin <93324329+DaniilKuzjomin@users.noreply.github.com>
2022-09-06 12:31:45 +0300

    save

--- esimene/Opilane.cs
+++ esimene/Opilane.cs
@@ -6,26 +6,30 @@ using System.Threading.Tasks;
 
 namespace esimene
 {
-     class Opilane : Isik
+    class Opilane : Isik
     {
 
         public string koolinimi;
-        public string klass;
+        public int klass;
         public string spetsialiseerimine;
 
         public Opilane(string koolinimi, int klass, string spetsialiseerumine, string nimi, int sunniaasta, sugu isikSugu, double maksuvaba, double palk) : base(nimi, sunniaasta, isikSugu, maksuvaba, palk)
+
         {
             this.koolinimi = koolinimi;
             this.klass = klass;
             this.spetsialiseerimine = spetsialiseerimine;
         }
 
-        public int arvutaVanus()
-        {
-            int praeguneAasta = DateTime.Now.Year;
-            int vanus = praeguneAasta - sunniaasta;
-            return vanus;
-        }
+        //public override int arvutaVanus()
+        //{
+        //    int praeguneAasta = DateTime.Now.Year;
+        //    int vanus = praeguneAasta - sunniaasta;
+        //    return vanus;
+        //}
+
+
+
 
         public override double arvSisse(double maksuvaba, double tulumaks, double palk)
         {
@@ -35,7 +39,7 @@ namespace esimene
 
         public override void printInfo()
         {
-            Console.WriteLine($"Tema koolinimi on {koolinimi} ja ta õpib klassis #{klass}, ja tema spetsialiseerimine on {spetsialiseerimine}, tema töötasu on {arvSisse(palk, maksuvaba, tulumaks)},tema nimi on {nimi}, ta on {InimSugu} ja tema vanus on {arvutaVanus()}");
+            Console.WriteLine($"Tema koolinimi on {koolinimi} ja ta õpib klassis #{klass}, ja tema spetsialiseerimine on {spetsialiseerimine}, tema töötasu on {arvSisse(palk, maksuvaba, tulumaks)},tema nimi on {nimi}, ta on {InimSugu} ja tema vanus on {base.arvutaVanus()}");
         }
     }
 }
